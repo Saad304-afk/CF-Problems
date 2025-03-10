65adf5857d5aee05fab50e8bd507e29c6e9382c1
Saad <saadbinmontasir11@gmail.com>
2025-03-10 17:31:35 +0600

    Add files via upload

---
+++ MIN&MAX.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+long long A,B,C;
+cin>>A>>B>>C;
+
+if(A<C)
+    {cout<<A<<" ";}
+   else if(B<A,C)
+        cout<<B<<" ";
+   else if(C<B,A)
+   {
+       cout<<C<<" ";
+   }
+else if(A<C)
+    cout<<A<<" ";
+     if(C>B)
+        cout<<C<<" ";
+    else if(B>A,C)
+        cout<<B ;
+
+
+}
