100b6647a1ceccdd0f90fb994d3ac3510a8d0181
Saad <saadbinmontasir11@gmail.com>
2025-03-10 17:39:20 +0600

    Add files via upload

---
+++ escape.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (){
+
+/* I am using escape sequence with comments
+as well , so lets see it :*/
+
+printf( "\a Hi there , \n I am Saad\n Now I am presenting\a ");
+
+printf("\a  \n lets do it by your potential \b \n this is the opportunity for you \n all the best \f   ");
+return 0;
+
+
+
+}
