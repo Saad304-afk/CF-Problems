d34636d5a43c6e7a34a81541116ce0bf8b182451
Saad <saadbinmontasir11@gmail.com>
2025-03-10 17:40:51 +0600

    Add files via upload

---
+++ problem e.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+
+int main (){
+    int R;
+    double area;
+
+    cin>>R;
+    area=  3.141592653*R*R;
+     cout<< fixed<<setprecision(9)<<area<<endl;
+     return (0);}
