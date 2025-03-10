e17a2adeba5c10f463b8aeb7111ea876d67e5e3c
Saad <saadbinmontasir11@gmail.com>
2025-03-10 17:33:11 +0600

    Add files via upload

---
+++ problem area.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+// finding area of a circle with 9 digits number after point.
+using namespace std;
+
+int main (){
+double R,area;
+cin>>R;
+area= 3.141592653*R*R;
+ cout<< fixed<<setprecision(9)<<area<<endl;
+
+
+
+
+
+
+}
