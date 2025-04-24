707675024a55f5ed3bbf4e0d18c59f22d7dee01b
Saad <saadbinmontasir11@gmail.com>
2025-04-24 12:50:37 +0600

    Add files via upload

---
+++ shopping.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   long long N;
+    scanf("%lld",&N);
+    if(N>=1000 && N<=1500)
+{printf("I will buy Punjabi");
+}
+else if(N>=1500)
+{ printf("I will buy Punjabi\nI will buy new shoes\nAlisa will buy new shoes");}
+
+else {printf("Bad luck!"); }
+
+}
