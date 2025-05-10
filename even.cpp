a659ed530abb66c0f710fc032c3328b9058599f9
Saad <saadbinmontasir11@gmail.com>
2025-05-10 20:10:08 +0600

    Add files via upload

---
+++ even.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{  long n;
+   cin>>n;
+   if(n==1)
+   {
+       cout<<"-1";
+   }
+   else{
+   for(long p=1;p<=n;p++)
+   {
+       //int r;
+       if(p%2==0)
+       { cout<<p<<endl;}
+       /*else if(p%2!=0)
+        {cout<<"-1";}*/
+   }}
+   /*else if(p%2!=0)
+        {cout<<"-1";}*/
+}
