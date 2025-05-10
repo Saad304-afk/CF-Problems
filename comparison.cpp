e00bb21653ccae9379ab0d26d534b0d97a847dcf
Saad <saadbinmontasir11@gmail.com>
2025-05-10 20:06:33 +0600

    Add files via upload

---
+++ comparison.cpp
@@ -0,0 +1,43 @@
+/*Given a number N
+.Determine whether N
+ is float number or integer number.
+ If N
+ is float number then print "float" followed by the integer part followed by decimal part separated by space.
+If N
+ is integer number then print "int" followed by the integer part separated by space.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
