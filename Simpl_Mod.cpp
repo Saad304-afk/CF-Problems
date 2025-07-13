d6c3fd983949d5ba912cb1cf6de47d2555573b36
Saad <saadbinmontasir11@gmail.com>
2025-07-13 23:04:52 +0600

    Add files via upload

---
+++ Simpl_Mod.cpp
@@ -0,0 +1,27 @@
+/*Hady has a positive number N
+and a simple equation:(X^2+Y^2)MOD N=0
+Your task is to find any values for X,Ythat satisfy the equation, such that X and Y are non-negative integers.
+Input:
+Only one integer N
+ (1≤N≤10^100).
+Output:
+If you can find any two non-negative integers such that (X,Y≤109), print them. Otherwise, print "No solutions".
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{ long long n;
+cin>>n;
+//x=0,y=0;
+/*for( x=1;x<=n;x++)
+{ for(y=1;y<=n;y++)
+    { //x=0,y=0;
+        if((x*x+y*y)%n==0)
+       {cout<<x<<" "<<y;
+return 0;}
+}
+
+}return 0;}*/
+cout<<"0 0"<<endl;}
