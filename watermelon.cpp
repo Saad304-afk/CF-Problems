9fe6354e7d137129075363e7a7e4cf2ec0b10728
Saad <saadbinmontasir11@gmail.com>
2025-07-28 00:35:45 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{ int w,rem=0;
+cin>>w;
+if(1<=w && w<=100)
+{ rem= w%2;
+  if(w==2)
+    cout<<"NO";
+   else if(rem ==0)
+  {
+      cout<<"YES";
+  }
+   else
+    {cout<<"NO";}}}
+
+
+
+
+
+
+
