fd57a04e566aa721810ae5d6418ec51110244451
Saad <saadbinmontasir11@gmail.com>
2025-08-07 00:52:19 +0600

    Add files via upload

---
+++ FizzBuzz.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ long long n;
+ cin>>n;
+ if(n%3==0 && n%5==0)
+ {cout<<"FizzBuzz";}
+ else if(n%5==0)
+ {cout<<"Buzz";}
+  else if(n%3==0)
+  {
+      cout<<"Fizz";
+  }
+  else if(n==1)
+    {cout<<" ";}
+  else
+  {cout<<" ";}
+
+
+
+}
